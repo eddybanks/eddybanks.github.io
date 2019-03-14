ef91a25acea59b30fff2a42e1c4a58cf28d50ff9
Edwin Agbenyega <eddybanks03@gmail.com>
2019-03-14 15:06:20 -0600

    Fix nav area click problem

--- src/components/layout/NavigationItem.js
+++ src/components/layout/NavigationItem.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 import styles from '../../stylesheets/Navigation.module.css'
 
 const navigationItem = props => (
-  <NavItem className={styles.NavigationItem}><NavLink><Link to={props.path}>{props.title}</Link></NavLink></NavItem>
+  <NavItem className={styles.NavigationItem}><Link to={props.path}><NavLink>{props.title}</NavLink></Link></NavItem>
 )
 
 export default navigationItem
