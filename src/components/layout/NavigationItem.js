cd9264c9e78330c0e6ca187f132166f7f0e8a4bf
Edwin Agbenyega <eddybanks03@gmail.com>
2019-03-14 15:17:29 -0600

    Try to figure out link's active styling without success

--- src/components/layout/NavigationItem.js
+++ src/components/layout/NavigationItem.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { NavItem, NavLink } from 'reactstrap'
-import { Link } from 'react-router-dom'
+import { NavLink as Link } from 'react-router-dom'
 import styles from '../../stylesheets/Navigation.module.css'
 
 const navigationItem = props => (
-  <NavItem className={styles.NavigationItem}><Link to={props.path}><NavLink>{props.title}</NavLink></Link></NavItem>
+  <NavItem className={styles.NavigationItem}><Link to={props.path} activeClassName={styles.Active}><NavLink>{props.title}</NavLink></Link></NavItem>
 )
 
 export default navigationItem
