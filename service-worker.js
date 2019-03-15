2755c063e9d436e26505ce519a1d9f82dce3bb86
Edwin Agbenyega <eddybanks03@gmail.com>
2019-03-15 17:55:10 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0cae7c3dc5835a092c0fa09d9580068a.js"
+  "/portfolio/precache-manifest.c930403648200d5447bb753ac81129be.js"
 );
 
 workbox.clientsClaim();
