6671d93280f6e6c289fcbdb731859f0b9d1344bb
Edwin Agbenyega <eddybanks03@gmail.com>
2019-03-15 17:54:29 -0600

    Style invisible scroll carousel for project component

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Project from './Project/Project'
+import styles from '../../stylesheets/Projects.module.css';
 
 const projects = (props) => {
   return (
-    <div>
-      {props.projects.map(project => <Project img={project.src} alt={project.alt} title={project.title} />)}      
+    <div className={styles.Projects}>
+      {props.projects.map(project => <Project img={project.src} alt={project.alt} title={project.title} key={project.id} />)}      
     </div>
   )
 }
--- src/components/projects/ProjectsContainer.js
+++ src/components/projects/ProjectsContainer.js
@@ -4,14 +4,25 @@ import Projects from './Projects'
 class ProjectsContainer extends Component {
   state = {
     projects: [
-      { img: {
+      { 
+        id: 0,
+        img: {
         src: 'http://i.imgur.com/B0gV7Gq.png',
         alt: 'Hangout@229'
         },
         title: 'Hangout@229'
+      },
+      { 
+        id: 1,
+        img: {
+        src: 'http://i.imgur.com/B0gV7Gq.png',
+        alt: 'Lippia Tea'
+        },
+        title: 'Lippia Tea'
       }
     ]
   }
+
   render() {
     return (
       <Projects projects={this.state.projects} />
