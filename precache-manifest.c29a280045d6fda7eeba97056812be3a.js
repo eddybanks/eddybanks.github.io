2f692b4815cec8d0bdca02b5c453b668eb06ab4d
Edwin Agbenyega <eddybanks03@gmail.com>
2019-03-14 20:52:36 -0600

    Updates

---
+++ precache-manifest.c29a280045d6fda7eeba97056812be3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a859744a352e1bc3ff1",
+    "url": "eddybanks.github.io/portfolio/static/css/main.3e7d7024.chunk.css"
+  },
+  {
+    "revision": "3a859744a352e1bc3ff1",
+    "url": "eddybanks.github.io/portfolio/static/js/main.265834d4.chunk.js"
+  },
+  {
+    "revision": "c3a19d61b87a51c94f55",
+    "url": "eddybanks.github.io/portfolio/static/js/runtime~main.409f0886.js"
+  },
+  {
+    "revision": "851d0806829d98762d87",
+    "url": "eddybanks.github.io/portfolio/static/css/2.8c6f1887.chunk.css"
+  },
+  {
+    "revision": "851d0806829d98762d87",
+    "url": "eddybanks.github.io/portfolio/static/js/2.67057a76.chunk.js"
+  },
+  {
+    "revision": "50a6389fadea2f85354f76d03e765c11",
+    "url": "eddybanks.github.io/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.26721a22e6788176d57b181e629c34f0.js"
+  "eddybanks.github.io/portfolio/precache-manifest.c29a280045d6fda7eeba97056812be3a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("eddybanks.github.io/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
