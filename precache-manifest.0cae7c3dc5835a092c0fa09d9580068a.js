a898e2c13f739661fbabb2d9ab56a44adbd72798
Edwin Agbenyega <eddybanks03@gmail.com>
2019-03-14 22:01:16 -0600

    Updates

---
+++ precache-manifest.0cae7c3dc5835a092c0fa09d9580068a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e562ddfa3e6466040a09",
+    "url": "/portfolio/static/css/main.3e7d7024.chunk.css"
+  },
+  {
+    "revision": "e562ddfa3e6466040a09",
+    "url": "/portfolio/static/js/main.71e34cd3.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "6d7dea49d6029f855a23",
+    "url": "/portfolio/static/css/2.8c6f1887.chunk.css"
+  },
+  {
+    "revision": "6d7dea49d6029f855a23",
+    "url": "/portfolio/static/js/2.505337c4.chunk.js"
+  },
+  {
+    "revision": "cc7d2b866e9725a7dd5fe622e6abdaf9",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8fb0e89fac8a3a51e1b2eac4922092fb.js"
+  "/portfolio/precache-manifest.0cae7c3dc5835a092c0fa09d9580068a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
