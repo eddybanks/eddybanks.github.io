5f23495b4abfb7b5f4588877181b39412f20b14c
Edwin Agbenyega <eddybanks03@gmail.com>
2019-03-27 13:05:15 -0600

    Add dates to education and work experience in resume

--- src/components/resume/ResumeContainer.js
+++ src/components/resume/ResumeContainer.js
@@ -21,18 +21,18 @@ class ResumeContainer extends Component {
           { title: 'Programming Languages: ', details: 'Python, Ruby, Java, C, C++, R (tidyverse)' },
           { title: 'Web Development: ', details: 'Ruby on rails, HTML5, CSS3, JQuery, React.js, Bootstrap, Webpack, GraphQL' },
           { title: 'Database Management and Data Analytics: ', details: 'PostgreSQL, Oracle, MySQL, MongoDB, Tableau' },
-          { title: 'Project Management & Version Control Tools: ', details: 'Slack, Trello, Asana, TargetProcess, Git, GitHub' }
+          { title: 'Project Management & Version Control Tools: ', details: 'Slack, Trello, Confluence, Git, GitHub' }
         ] 
       },
       { id: 3, type: 'section', title: 'Work Experience', 
         content: [
-          { title: 'Institutional Researcher: ', details: 'University of New Mexico', start_date: '02.27.2017', end_date: 'Ongoing' },
-          { title: 'Research Assistant: ', details: 'University of New Mexico', start_date: '01.09.2015', end_date: '12.16.2016' }
+          { title: 'Institutional Researcher: ', details: 'University of New Mexico (Feb 27, 2017 ~ present)', start_date: 'Feb 27, 2017', end_date: 'Ongoing' },
+          { title: 'Research Assistant: ', details: 'University of New Mexico (Feb 11, 2015 ~ Dec 16, 2016)', start_date: 'Feb 11, 2015', end_date: 'Dec 16, 2016' }
         ] },
       { id: 4, type: 'section', title: 'Education', 
         content: [
-          { title: 'MSc. Computer Engineering: ', details: 'University of New Mexico', start_date: '01.09.2015', end_date: '06.31.2017' },
-          { title: 'BSc. Computer Engineering: ', details: 'Chungnam National University', start_date: '03.20.2011', end_date: '12.16.2014' }
+          { title: 'MSc. Computer Engineering: ', details: 'University of New Mexico (Jan 15, 2015 ~ Jul 31, 2017)', start_date: 'Jan 15, 2015', end_date: 'Jul 31, 2017' },
+          { title: 'BSc. Computer Engineering: ', details: 'Chungnam National University (Mar 11, 2011 ~ Feb 25, 2015)', start_date: 'Mar 11, 2011', end_date: 'Feb 25, 2015' }
         ] },
       { id: 5, type: 'section', title: 'Languages', 
         content: [
