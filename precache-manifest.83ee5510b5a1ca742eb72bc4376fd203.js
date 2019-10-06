3ce8dc968927b7781d148af0df4ab5d2a5b59779
Edwin Agbenyega <eddybanks@unm.edu>
2019-10-05 22:23:51 -0600

    Updates

---
+++ precache-manifest.83ee5510b5a1ca742eb72bc4376fd203.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf672c92dcae541aa1d663ad9267a37b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cf24157ad40458689f8b",
+    "url": "/portfolio/static/css/2.619dee99.chunk.css"
+  },
+  {
+    "revision": "525047001335f2dca58c",
+    "url": "/portfolio/static/css/main.3be2297f.chunk.css"
+  },
+  {
+    "revision": "cf24157ad40458689f8b",
+    "url": "/portfolio/static/js/2.07e15369.chunk.js"
+  },
+  {
+    "revision": "525047001335f2dca58c",
+    "url": "/portfolio/static/js/main.01abfccb.chunk.js"
+  },
+  {
+    "revision": "a46b3fd8f34f8691f45e",
+    "url": "/portfolio/static/js/runtime-main.6c1b9ebb.js"
+  },
+  {
+    "revision": "2c1f4e93695b2a3380c8513a65ee3178",
+    "url": "/portfolio/static/media/background.2c1f4e93.jpg"
+  },
+  {
+    "revision": "18f344e6f0cd5190dd1ef30ce1bddd5f",
+    "url": "/portfolio/static/media/background3.18f344e6.png"
+  }
+]);
