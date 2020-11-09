3c08c28d2a56e8637c5296d3c43821f469bce0da
Edwin Agbenyega <eddybanks03@gmail.com>
2020-11-09 16:18:48 -0700

    Temporarily remove incomplete projects

--- src/components/projects/ProjectsContainer.js
+++ src/components/projects/ProjectsContainer.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react'
 import Projects from './Projects'
 import { Container } from 'reactstrap'
 import schooladmin from '../../assets/schooladmin.png'
-import hangout229 from '../../assets/hangout229.png'
-import kirtlandlogistics from '../../assets/kirtlandlogistics.png'
-import portfolio from '../../assets/portfolio.png'
+// import hangout229 from '../../assets/hangout229.png'
+// import kirtlandlogistics from '../../assets/kirtlandlogistics.png'
+// import portfolio from '../../assets/portfolio.png'
 import crwneddy from '../../assets/crwneddy.png'
 
 class ProjectsContainer extends Component {
@@ -28,33 +28,33 @@ class ProjectsContainer extends Component {
         title: 'Crwn Clothing',
         url: 'https://crwneddy.herokuapp.com/'
       },
-      { 
-        id: 2,
-        img: {
-        src: hangout229,
-        alt: 'Hangout@229'
-        },
-        title: 'Hangout@229',
-        url: 'https://eddybanks.github.io/hangout229'
-      },
-      { 
-        id: 3,
-        img: {
-        src: portfolio,
-        alt: 'Lippia Tea'
-        },
-        title: 'Portfolio Site',
-        url: 'https://eddybanks.github.io/portfolio'
-      },
-      { 
-        id: 4,
-        img: {
-        src: kirtlandlogistics,
-        alt: 'Kirtland Logistics'
-        },
-        title: 'Kirtland Logistics',
-        url: 'https://eddybanks.github.io/kirtland-logistics'
-      },
+      // { 
+      //   id: 2,
+      //   img: {
+      //   src: hangout229,
+      //   alt: 'Hangout@229'
+      //   },
+      //   title: 'Hangout@229',
+      //   url: 'https://eddybanks.github.io/hangout229'
+      // },
+      // { 
+      //   id: 3,
+      //   img: {
+      //   src: portfolio,
+      //   alt: 'Portfolio Site'
+      //   },
+      //   title: 'Portfolio Site',
+      //   url: 'https://eddybanks.github.io/portfolio'
+      // },
+      // { 
+      //   id: 4,
+      //   img: {
+      //   src: kirtlandlogistics,
+      //   alt: 'Kirtland Logistics'
+      //   },
+      //   title: 'Kirtland Logistics',
+      //   url: 'https://eddybanks.github.io/kirtland-logistics'
+      // },
       // { 
       //   id: 5,
       //   img: {
@@ -71,6 +71,7 @@ class ProjectsContainer extends Component {
     return (
       <Container>
         <Projects projects={this.state.projects} />
+        <hr />
         <p>You can follow my <a href='https://github.com/eddybanks' target='_blank' rel='noopener noreferrer'>github</a> to check out other projects I'm working on!!</p>
       </Container>
     )
